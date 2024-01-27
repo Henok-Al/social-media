1fdb824fb3252d0f4bd3e926f61fb23fdbabfd32
bebetterthinker <henok.gebresenbet@gmail.com>
2024-01-27 12:15:51 +0300

    impolementing authetication,autherization, user routes

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,8 +1,8 @@
 import bcrypt from "bcrypt";
-// import { Jwt } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
-// REGISTER USER
+/* REGISTER USER */
 export const register = async (req, res) => {
   try {
     const {
@@ -31,9 +31,26 @@ export const register = async (req, res) => {
       viewedProfile: Math.floor(Math.random() * 10000),
       impressions: Math.floor(Math.random() * 10000),
     });
-
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+/* LOGGING IN */
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (!user) return res.status(400).json({ msg: "User does not exist. " });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials. " });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    delete user.password;
+    res.status(200).json({ token, user });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
