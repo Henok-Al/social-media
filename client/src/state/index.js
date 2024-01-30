6c22b309eee3285bb21fa6a86358e28225c05abf
bebetterthinker <henok.gebresenbet@gmail.com>
2024-01-30 11:12:35 +0300

    implementing navbar

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -13,7 +13,7 @@ export const authSlice = createSlice({
   //these are functions that are modifiying the global state
   reducers: {
     setMode: (state) => {
-      state.mode = state.mode == "light" ? "dark" : "light";
+      state.mode = state.mode === "light" ? "dark" : "light";
     },
     setLogin: (state, action) => {
       state.user = action.payload.user;
