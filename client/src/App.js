c3fdfa65cdd0bb20951181563bb77e958fc9e851
bebetterthinker <henok.gebresenbet@gmail.com>
2024-01-30 10:52:41 +0300

    implementing color,theme,dark mode

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,27 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "scenes/homePage";
 import LoginPage from "scenes/loginPage";
 import ProfilePage from "scenes/profilePage";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { themeSettings } from "./theme";
 
 function App() {
+  //used to grap the value initial state
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createtheme(themeSettings(mode)), [mode]);
+
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/profile/:userId" element={<ProfilePage />} />
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
